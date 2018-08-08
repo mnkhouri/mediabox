505f603fb66bc75fb3292c41f64bbd75e849c939
Marc Khouri <marc@khouri.ca>
2018-08-08 00:36:50 -0400

    Fix check-docker script

--- checkDocker.sh
+++ checkDocker.sh
@@ -6,11 +6,14 @@ if ps ax | grep "[D]ocker.app" >/dev/null; then
     if ! /usr/local/bin/docker ps 2>&1 1>/dev/null; then
        echo "'docker ps' failed, killing docker"
         ps ax | grep "[dD]ocker"
-        kill -9 $(pgrep docker) || true
-        kill -9 $(pgrep Docker)
+        pgrep "[dD]ocker" | grep -v ${PPID}
+        kill -9 $(pgrep "[dD]ocker" | grep -v ${PPID})
         sleep 5
+        kill -9 $(pgrep "[dD]ocker" | grep -v ${PPID})
         echo "Starting docker"
-        open /Applications/Docker.app || true
+        open /Applications/Docker.app
+        ps ax | grep "[dD]ocker"
+        pgrep "[dD]ocker" | grep -v ${PPID}
         echo "Started docker with code $?"
     else
         echo "docker ps is responding"
